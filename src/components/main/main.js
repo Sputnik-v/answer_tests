3a1cc33077946e30cef755ecb15e30b0128af8cf
Igor Tronin <igortronin2705@gmail.com>
2022-04-04 16:10:19 +0400

    main-header

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,8 +4,10 @@ import './main.css';
 const Main = () => {
     return (
         <div className="main">
-            <p>Give you tabs for button for top list</p>
-        </div>
+            <p>Choose a topic for our quiz</p>
+            <span className="shapka"></span>
+
+        </div>    
     )
 }
 
